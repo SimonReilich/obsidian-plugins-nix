50b1c949816e4c8b5e7ee7469ca03ffd6f5e9247
SimonReilich <simon.reilich@proton.me>
2026-02-16 16:03:25 +0100

    updating obsidian-git to 2.37.0

--- git.nix
+++ git.nix
@@ -6,22 +6,22 @@
 
 pkgs.stdenv.mkDerivation rec {
   pname = "obsidian.plugins.git";
-  version = "2.36.0";
+  version = "2.37.0";
   repo = "https://github.com/Vinzent03/obsidian-git";
 
   mainJs = pkgs.fetchurl {
     url = "${repo}/releases/download/${version}/main.js";
-    sha256 = "sha256-Xk2bQZDZMhPdWylkVSZCI2XgsS4ug+hZKbjlAL9EgBE=";
+    sha256 = "sha256-VIhuNzf2AnP7rxIpZAudDHBIl7Mihr26DkHyV+VdLj4=";
   };
 
   manifest = pkgs.fetchurl {
     url = "${repo}/releases/download/${version}/manifest.json";
-    sha256 = "sha256-vh1/gqPjttVUROqCKbIry388gSjjCQQBK3q8uJGGJos=";
+    sha256 = "sha256-ARwB0I1rfebPfB9U7XJ9I+CWqtUTnsNWAkoCnRidtvo=";
   };
 
   stylesCss = pkgs.fetchurl {
     url = "${repo}/releases/download/${version}/manifest.json";
-    sha256 = "sha256-vh1/gqPjttVUROqCKbIry388gSjjCQQBK3q8uJGGJos=";
+    sha256 = "sha256-ARwB0I1rfebPfB9U7XJ9I+CWqtUTnsNWAkoCnRidtvo=";
   };
 
   phases = [ "installPhase" ];
